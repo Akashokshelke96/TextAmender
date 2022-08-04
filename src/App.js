9fec8c595b49a57095116a130f3a8bce34d664d3
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-08-04 23:09:41 +0530

    added Alert functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 
+import { useState } from 'react';
 import './App.css';
+import Alert from './Components/Alert';
 import './Components/Navbar.js'
 import Navbar from './Components/Navbar.js';
 import TextForm from './Components/TextForm';
 
 
+
 function App() {
+
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const [alert, setAlert] = useState(null)
+
+  const showAlert=(message,type)=>{
+     setAlert({
+      msg : message,
+      type:type
+     })
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been unabled","success")
+    }
+    else{
+      setMode('light');
+      showAlert("Light mode has been unabled","success")
+
+    }
+  }
+
   return (
 <>
-<Navbar title="TextUtils" aboutText="About Us"/>
-<div className='container'>
-<TextForm heading="Enter text.."/>
-</div>
-
-
+ {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    </div>
   </>
 
   );
